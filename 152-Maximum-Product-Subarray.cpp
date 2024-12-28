285072d02958159b598b60d7f292c599956b1891
Abhimanyu Sharma <115303576+Abhimanyu254a@users.noreply.github.com>
2024-12-28 21:37:59 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 42.76%] - LeetPush

---
+++ 152-Maximum-Product-Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max_product = nums[0], min_product = nums[0], global_max = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            int current = nums[i];
+            
+            // Store max_product temporarily as it will be updated
+            int temp_max = max({current, current * max_product, current * min_product});
+            min_product = min({current, current * max_product, current * min_product});
+            
+            max_product = temp_max;
+            global_max = max(global_max, max_product);
+        }
+
+        return global_max;
+    }
+};
